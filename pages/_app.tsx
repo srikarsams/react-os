621f913c17860b251baf40b17352ddc78468175a
srikar-samudrala <srikar@eve.world>
2022-05-25 18:54:37 +0530

    adds eslint base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "styles/globals.scss";
+import 'styles/globals.scss';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
