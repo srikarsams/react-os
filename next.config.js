e2c8a993af0ab77d6ee4d4b5cfc82e8a50c38bb1
srikar-samudrala <srikar@eve.world>
2022-05-25 19:37:42 +0530

    adds stylelint support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, options) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  },
 }
 
 module.exports = nextConfig
