6ad029ce59b0331b62fc96082ecb79bfe384bc2a
srikar-samudrala <srikar@eve.world>
2022-05-26 16:27:39 +0530

    adds custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import 'styles/globals.scss';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>React OS</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
